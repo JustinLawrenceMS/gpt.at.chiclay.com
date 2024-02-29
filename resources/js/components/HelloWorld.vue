463b71c0ca115ddca4bdfdd7267f24d60655b878
Justin Lawrence <43936909+JustinLawrenceMS@users.noreply.github.com>
2024-02-29 00:09:46 -0600

    Update HelloWorld.vue

--- resources/js/components/HelloWorld.vue
+++ resources/js/components/HelloWorld.vue
@@ -2,9 +2,9 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      This is the scoobiest GPT of all the JustinGPTs<br>
-      check out the
-      <a href="https://justin.at.chiclay.com" target="_blank" rel="noopener">Creators</a>.
+      This is the scoobiest GPT of all the JustinGPTs.<br>
+      Check out the
+      <a href="https://linkedin.com/company/chiclay-s/" target="_blank" rel="noopener">Creators</a>.
     </p>
   </div>
 </template>
